66a6da63405346c47a80812be939877ae042c58c
Hariraj3 <hariraj@gmail.com>
2015-10-30 06:36:33 -0700

    KPI update

---
+++ Vz.MegaHack.Test/EvaluationEngineTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Vz.MegaHack.Engines;
+using System.IO;
+using Vz.MegaHack.Core;
+
+namespace Vz.MegaHack.Test
+{
+    [TestFixture]
+    public class EvaluationEngineTest
+    {
+        [Test]
+        public void GetLeaderBoard()
+        {
+            PathManager.Initialize(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data"));
+            int intLeaderBoardCount = 0;
+            intLeaderBoardCount = EvaluationEngine.GetLeaderBoard("001").Count;
+            Assert.LessOrEqual(12, intLeaderBoardCount);
+        }
+    }
+}
